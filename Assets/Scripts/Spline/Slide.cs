6eca309bac8d76c2a9e1bf376d395e3d0e77ab76
Zoeghs <pilgrimz@sheridancollege.ca>
2021-08-25 18:26:24 -0400

    Spline Upgrade
    - Now, when the object moving along the spline hits the end, it moves in the other direction.  The direction will keep flipping when it hits either end.
    - The movement speed of the object along the spline is now faster and tied to a variable.

--- Assets/Scripts/Spline/Slide.cs
+++ Assets/Scripts/Spline/Slide.cs
@@ -1,6 +1,10 @@
 // This script is by AxiomaticUncertainty
 // Can be found at: https://www.youtube.com/watch?v=T8pZiWQZ63g
 
+// This script is to be put on ojects that move along the spline
+
+// Modified by Zoe Pilgrim to reverse movement of the object along the spline, instead of resetting it to the starting point, once it reaches the end.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +14,12 @@ public class Slide : MonoBehaviour
     public float t = 0f;
     public SplineManager spline;
 
+    // Direction bool
+    private bool movePositive = true;
+
+    // What to divide delta time by (speed: lower to go faster, higher to go slower)
+    private float speedDivisor = 5f;
+
     void Start()
     {
         t = 0;
@@ -18,12 +28,41 @@ public class Slide : MonoBehaviour
     void Update()
     {
         this.transform.position = spline.getSplinePosition(t);
-        t += Time.deltaTime / 10f;
 
-        if (t > 1)
+        // Find direction object needs to be moving in (positive of negative)
+        FindDirection();
+
+        // Move time according to the specified direction
+        MoveTime();
+    }
+
+    private void FindDirection()
+    {
+        // When the object hits the end of the spline
+        if (t >= 1)
         {
-            // Return back to 0 (restart to beginning of spline)
-            t = 0;
+            // Decrease time, instead of increase time to make the object move in reverse
+            movePositive = false;
+        }
+        // If the object hits the bottom on the spline
+        else if (t <= 0)
+        {
+            // Increase time to move toawrds the end of the spline
+            movePositive = true;
+        }
+    }
+
+    private void MoveTime()
+    {
+        // If the object needs to move in a positive direction
+        if (movePositive == true)
+        {
+            t += Time.deltaTime / speedDivisor;
+        }
+        // If the object needs to move in a negative direction
+        else
+        {
+            t -= Time.deltaTime / speedDivisor;
         }
     }
 }
--- Assets/Scripts/Spline/Vertex.cs
+++ Assets/Scripts/Spline/Vertex.cs
@@ -1,6 +1,8 @@
 // This script is by AxiomaticUncertainty
 // Can be found at: https://www.youtube.com/watch?v=T8pZiWQZ63g
 
+// This script is to be attached to objects that are vertex points along the spline
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
