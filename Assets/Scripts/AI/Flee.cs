4059f948267190de5fa5eb60be3391af3e60eece
Zoeghs <pilgrimz@sheridancollege.ca>
2021-12-08 01:15:23 -0500

    Fixed: AI Can Hear Player
    - AI properly reacts to hearing the player at different ranges and noise levels. (Using distence checks instead of colliders.)
    - Removed corutines.
    - Preped script for proper state changes.
    - Still hasbug where AI does not flee for the apropriate amount of time, but possible solutions have been identified.

--- Assets/Scripts/AI/Flee.cs
+++ Assets/Scripts/AI/Flee.cs
@@ -5,6 +5,10 @@ using UnityEngine.AI;
 
 public class Flee : MonoBehaviour
 {
+    #region Variables
+
+    public string state = "";
+
     // What to flee from
     [SerializeField] bool fleeFromPlayer;
     [SerializeField] bool fleeFromPredators;
@@ -13,7 +17,7 @@ public class Flee : MonoBehaviour
     private NavMeshAgent navAgent;
 
     // How close the player / predator has to be in order to run away
-    private float fleeDistance = 50f;
+    private float fleeTime = 10f;
 
     // Speed at which the AI object can run away
     //private float fleeSpeed = 6f;
@@ -21,18 +25,23 @@ public class Flee : MonoBehaviour
     // Vars for hearing and vision colliders
     [SerializeField] GameObject hearingRange;
     [SerializeField] GameObject visionCone;
-    private AIHearing hearingScript;
     private AIVision visionScript;
 
     // Distance vars
     private float distanceFromPlayer;
     private float distanceFromPredator;
 
-    // Corutine bool to make sure it isn't running mutiple of itself
-    private bool currentlyFleeing = false;
-
     // Access to sound controller
     private SoundController soundController;
+
+    // Hearing range distances
+    private float outerHearingRange = 12;
+    private float innerHearingRange = 7;
+
+    // If the AI is currently fleeing
+    private bool currentlyFleeing = false;
+
+    #endregion
 
 
     void Start()
@@ -42,7 +51,7 @@ public class Flee : MonoBehaviour
 
         // Get vision & hearing scripts
         visionScript = visionCone.GetComponent<AIVision>();
-        hearingScript = hearingRange.GetComponent<AIHearing>();
+        //hearingScript = hearingRange.GetComponent<AIHearing>();
 
         // Find sound controller in the scene
         soundController = FindObjectOfType<SoundController>();
@@ -54,29 +63,62 @@ public class Flee : MonoBehaviour
         distanceFromPlayer = CalculateDistance("Player");
         distanceFromPredator = CalculateDistance("Predator");
 
-        // If this object needs to flee from the player
-        if (fleeFromPlayer == true && visionScript.playerIsSeen == true && currentlyFleeing == false)
-        {
-            StartCoroutine(nameof(FleeFrom), "Player");
-        }
-        else if (fleeFromPlayer == true && hearingScript.playerIsHeardIn && soundController.totalLoudness > 0.005f && currentlyFleeing == false)
-        {
-            StartCoroutine(nameof(FleeFrom), "Player");
-        }
-        // if the AI hears the player, turn towards them
-        else if (fleeFromPlayer == true && hearingScript.playerIsHeardOut == true && soundController.totalLoudness > 0.005f)
-        {
-            TurnTowards("Player");
-        }
-        else if (fleeFromPlayer == true && hearingScript.playerIsHeardIn == true && soundController.totalLoudness < 0.005f)
-        {
-            TurnTowards("Player");
-        }
+        // Check if player is within hearing ranges
+        HearingVisionCheck("Player", distanceFromPlayer);
 
-        // If this object needs to flee from predators
-        if (fleeFromPredators == true && distanceFromPredator <= fleeDistance && currentlyFleeing == false)
+        // Check if predators are withing hearing ranges
+        // WILL CURRENTLY NOT WORK BECAUSE TOTAL LOUDNESS ONLY APPLIES TO THE PLAYER
+
+        Debug.Log("state of animalia: " + state);
+    }
+
+    private void updateState()
+    {
+        // Use switches here
+
+        // when a state finishes running its code, switch to the default state
+        // Default state looks for what state it needs to switch into
+    }
+
+
+
+    private void HearingVisionCheck(string targetTag, float distance)
+    {
+        GameObject player = GameObject.FindGameObjectWithTag(targetTag);
+
+        // If the player is in the outer hearing range and is making less than 0.005 in noise, no reaction from AI
+
+        // If the player is in the outer hearing range
+        if (distance < outerHearingRange && distance > innerHearingRange)
         {
-            StartCoroutine(nameof(FleeFrom), "Predator");
+            // If the player is making greater than 0.005 in noise, the AI turns towards the player
+            if (soundController.totalLoudness > 0.005f && currentlyFleeing == false)
+            {
+                TurnTowards(targetTag);
+                state = "TURNING";
+            }
+        }
+        // If the player is in the inner hearing range
+        else if (distance < innerHearingRange)
+        {
+            // If the player is in the AI vision cone (this behaviour is checked first as it overrides any sound behaviours)
+            if (visionScript.playerIsSeen == true && currentlyFleeing == false)
+            {
+                FleeFrom(targetTag);
+                state = "FLEEING";
+            }
+            // If the player is making less than 0.005 in noise, the AI turns towards the player
+            else if (soundController.totalLoudness < 0.005f && currentlyFleeing == false)
+            {
+                TurnTowards(targetTag);
+                state = "TURNING";
+            }
+            // If the player is making greater than 0.005 in noise, the AI turns away from the player and runs
+            else if (soundController.totalLoudness < 0.005f && currentlyFleeing == false)
+            {
+                FleeFrom(targetTag);
+                state = "FLEEING";
+            }
         }
     }
 
@@ -87,32 +129,33 @@ public class Flee : MonoBehaviour
         gameObject.transform.LookAt(subject.transform);
     }
 
-    IEnumerator FleeFrom(string fleeTag)
+    private void FleeFrom(string fleeTag)
     {
-        // Only run if not currenly running
+        // Set current time
+        float currentFleeTime = fleeTime;
+
         currentlyFleeing = true;
 
-        distanceFromPlayer = CalculateDistance("Player");
-
-        // Get specified object to flee from
-        GameObject toFleeFrom = GameObject.FindGameObjectWithTag(fleeTag);
-
-        // Find vector from specified object to this object
-        Vector3 distanceVector = gameObject.transform.position - toFleeFrom.transform.position;
-
-        // Calcualte new position
-        Vector3 newPos = gameObject.transform.position + distanceVector;
-
-        // Move the object away from the specified object
-        navAgent.SetDestination(newPos);
-
-        // If the AI is far away enough from the player, it stops running
-        if (distanceFromPlayer > fleeDistance)
+        // Loop for how long is specified
+        for (int i = 0; i < currentFleeTime; i++)
         {
-           currentlyFleeing = false;
+            // Get specified object to flee from
+            GameObject toFleeFrom = GameObject.FindGameObjectWithTag(fleeTag);
+
+            // Find vector from specified object to this object
+            Vector3 distanceVector = gameObject.transform.position - toFleeFrom.transform.position;
+
+            // Calcualte new position
+            Vector3 newPos = gameObject.transform.position + distanceVector;
+
+            // Move the object away from the specified object
+            navAgent.SetDestination(newPos);
+
+            // Update time
+            currentFleeTime -= Time.deltaTime;
         }
 
-        yield return null;
+        currentlyFleeing = false;
     }
 
     private float CalculateDistance(string distanceTag)
